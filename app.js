d9b6aa38662c142f706bea089206855fc5c19117
firatkocoglu <fratkocogl@gmail.com>
2025-03-24 19:08:16 +0300

    Neon db connection is implemented.

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
+const app = express();
+const morgan = require('morgan');
+const { Pool } = require('pg');
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Library Management Application');
+app.use(morgan('dev'));
+
+app.get('/', async (req, res) => {
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+  const client = await pool.connect();
+  const result = await client.query(
+    'SELECT name, author, year, genre FROM books INNER JOIN genres ON books.genre_id = genres.id'
+  );
+  client.release();
+  const { rows } = result;
+  if (rows) {
+    res.json(rows);
+  } else {
+    res.send('No books found');
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(
+    `Connected to Neon Database. Server is running on port ${port}. Visit http://localhost:${port}.`
+  );
 });
