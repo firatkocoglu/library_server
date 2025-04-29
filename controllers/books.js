b19256258cc3fe46a69070b7a8048c9d868c1e47
firatkocoglu <fratkocogl@gmail.com>
2025-04-29 14:52:52 +0300

    Add and delete functions for admins are implemented

--- controllers/books.js
+++ controllers/books.js
@@ -21,7 +21,10 @@ const getBookByID = async (req, res) => {
   const { id } = req.params;
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM books WHERE id= $1', [id]);
+    const result = await client.query(
+      'SELECT * FROM books INNER JOIN genres ON books.genres_id=genres.id WHERE id= $1',
+      [id]
+    );
     client.release();
     const { rows } = result;
     if (rows.length > 0) {
@@ -35,4 +38,84 @@ const getBookByID = async (req, res) => {
   }
 };
 
-module.exports = { getBooks };
+const addBook = async (req, res) => {
+  const { name, author, genre_id, year, image_url } = req.body;
+  if (!name || !author || !genre_id || !year) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
+  try {
+    const client = await pool.connect();
+    const result = await client.query(
+      `INSERT INTO books (name, author, year, genre_id, image_url) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+      [name, author, year, genre_id, image_url]
+    );
+    client.release();
+    const { rows } = result;
+    if (rows.length > 0) {
+      res.status(201).json(rows[0]);
+    } else {
+      res.status(400).json({ message: 'Error adding book' });
+    }
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: 'Internal Server Error adding book' });
+  }
+};
+
+const updateBook = async (req, res) => {
+  const { id } = req.params;
+  const fields = req.body;
+
+  const keys = Object.keys(fields);
+  const values = Object.values(fields);
+
+  if (keys.length === 0) {
+    return res.status(400).json({ message: 'No fields to update' });
+  }
+
+  const setClause = keys
+    .map((key, index) => `${key} = $${index + 1}`)
+    .join(', ');
+
+  const query = `UPDATE books SET ${setClause} WHERE id = $${
+    values.length + 1
+  } RETURNING *`;
+
+  values.push(id);
+
+  try {
+    const client = await pool.connect();
+    const result = await client.query(query, values);
+    client.release();
+    const { rows } = result;
+    if (rows.length > 0) {
+      res.json(rows[0]);
+    } else {
+      res.status(404).json({ message: 'No book found with given id' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error updating book' });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const client = await pool.connect();
+    const result = await client.query('DELETE FROM books WHERE id = $1', [id]);
+    client.release();
+    if (result.rowCount > 0) {
+      res.status(200).json({ message: 'Book deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'No book found with given id' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error deleting book' });
+  }
+};
+
+module.exports = { getBooks, getBookByID, updateBook, deleteBook, addBook };
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,9 +1,19 @@
-const { getBooks } = require('../controllers/books.js');
+const {
+  getBooks,
+  getBookByID,
+  addBook,
+  updateBook,
+  deleteBook,
+} = require('../controllers/books.js');
 const { Router } = require('express');
 const { isAuthenticated, isAdmin } = require('../middleware/auth.js');
 
 const bookRouter = Router();
 
 bookRouter.get('/', getBooks);
+bookRouter.get('/:id', getBookByID);
+bookRouter.post('/', isAuthenticated, isAdmin, addBook);
+bookRouter.patch('/:id', isAuthenticated, isAdmin, updateBook);
+bookRouter.delete('/:id', isAuthenticated, isAdmin, deleteBook);
 
 module.exports = bookRouter;
