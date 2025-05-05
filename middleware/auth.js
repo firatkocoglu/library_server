e5723e3c7d5426e6289a16abf7d368810f399338
firatkocoglu <fratkocogl@gmail.com>
2025-05-05 18:57:29 +0300

    Fix typo in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -49,26 +49,22 @@ const isAdmin = async (req, res, next) => {
     return res.status(401).json({ message: 'Unauthorized' });
   }
 
-  const decoded = jwt.verify(
-    token,
-    process.env.JWT_SECRET,
-    function (err, dcd) {
-      if (err) {
-        if (err.name === 'TokenExpiredError') {
-          return res.status(401).json({ message: 'Token expired' });
-        }
-        return res.status(403).json({ message: 'Invalid token' });
-      } else {
-        if (dcd.isAdmin === false) {
-          return res
-            .status(403)
-            .json({ message: 'You are not authorized to this operation.' });
-        }
-        req.user = dcd;
-        next();
+  jwt.verify(token, process.env.JWT_SECRET, function (err, dcd) {
+    if (err) {
+      if (err.name === 'TokenExpiredError') {
+        return res.status(401).json({ message: 'Token expired' });
       }
+      return res.status(403).json({ message: 'Invalid token' });
+    } else {
+      if (dcd.isAdmin === false) {
+        return res
+          .status(403)
+          .json({ message: 'You are not authorized to this operation.' });
+      }
+      req.user = dcd;
+      next();
     }
-  );
+  });
 };
 
 module.exports = {
